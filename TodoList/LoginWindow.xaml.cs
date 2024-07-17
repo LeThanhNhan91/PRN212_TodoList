799116e5d1479f3048880588fdb4ee83517a3c85
ngocdt18 <ngocdtse183959@fpt.edu.vn>
2024-07-17 17:09:10 +0700

    Login page redirect to admin only

---
+++ TodoList/LoginWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+	/// <summary>
+	/// Interaction logic for LoginWindow.xaml
+	/// </summary>
+	public partial class LoginWindow : Window
+	{
+		private UserService _service = new();
+		public LoginWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void LoginButton_Click(object sender, RoutedEventArgs e)
+		{
+			string email = EmailTextBox.Text;
+			string password = PasswordTextBox.Text;
+			if (email.IsNullOrEmpty() || password.IsNullOrEmpty())
+			{
+				MessageBox.Show("Email or password is empty!", "Field required", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+			var user = _service.Login(email, password);
+			if (user == null)
+			{
+				MessageBox.Show("Wrong email or password!", "Incorrect credential", MessageBoxButton.OK);
+				return;
+			}
+			this.Hide();
+			switch (user.Role)
+			{
+				case 0: (new AdminWindow()).ShowDialog(); break;
+				case 1: break;
+				default: 
+					MessageBox.Show("You do not have the permission for the system", "Not allowed", MessageBoxButton.OK, MessageBoxImage.Error); 
+					break;
+			}
+		}
+	}
+}
